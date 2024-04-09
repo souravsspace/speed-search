8031736b72d77fa1cd7eea0630bbb4bf7156eb47
Sourav <souravsspace@gmail.com>
2024-04-10 03:37:14 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,123 @@
-import Image from "next/image";
+"use client"
+
+import { Button } from "@/components/button"
+import {
+   Command,
+   CommandEmpty,
+   CommandGroup,
+   CommandInput,
+   CommandItem,
+   CommandList,
+} from "@/components/ui/command"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   const [query, setQuery] = useState("")
+   const [searchResults, setSearchResults] = useState<{
+      results: string[]
+      duration: number
+   }>()
+   const [isMounted, setIsMounted] = useState(false)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+   useEffect(() => {
+      setIsMounted(true)
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      const fetchData = async () => {
+         if (!query) return setSearchResults(undefined)
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+         const res = await fetch(
+            `https://speed-search.souravsspace.workers.dev/api/search?q=${query}`
+         )
+         const data = (await res.json()) as {
+            results: string[]
+            duration: number
+         }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+         setSearchResults(data)
+      }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      fetchData()
+   }, [query])
+
+   if (!isMounted) return null
+
+   return (
+      // <main className="h-screen w-screen grainy">
+      <main className="h-screen w-screen">
+         <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+            <h1 className="text-5xl tracking-tight font-bold">
+               SpeedSearch ⚡
+            </h1>
+            <article className="text-muted-foreground text-lg max-w-prose text-center">
+               A high-performance API built with Hono, Next.js and Cloudflare.{" "}
+               <br /> Type a query below and get your results in miliseconds.
+            </article>
+
+            <div className="max-w-md w-full">
+               <Dialog>
+                  <DialogTrigger asChild>
+                     <Button
+                        variant="outline"
+                        className="w-full flex items-center justify-between"
+                     >
+                        <h4>Search Country...</h4>
+
+                        <Button
+                           variant="secondary"
+                           size="icon"
+                           className="w-8 h-7 px-2.5"
+                        >
+                           <span className="text-xs">⌘ K</span>
+                        </Button>
+                     </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                     <Command>
+                        <CommandInput
+                           value={query}
+                           onValueChange={setQuery}
+                           placeholder="Search countries..."
+                           className="placeholder:text-zinc-500 dark:placeholder:text-zinc-300"
+                        />
+
+                        <CommandList>
+                           {searchResults?.results.length === 0 ? (
+                              <CommandEmpty>No results found</CommandEmpty>
+                           ) : null}
+
+                           {searchResults?.results ? (
+                              <CommandGroup
+                                 heading={`Results for '${query}'`}
+                                 className="text-muted-foreground"
+                              >
+                                 {searchResults?.results.map((result) => (
+                                    <CommandItem
+                                       key={result}
+                                       value={result}
+                                       onSelect={setQuery}
+                                    >
+                                       {result}
+                                    </CommandItem>
+                                 ))}
+                              </CommandGroup>
+                           ) : null}
+
+                           {searchResults?.results ? (
+                              <>
+                                 <div className="h-px w-full bg-zinc-200 dark:bg-zinc-400" />
+                                 <article className="p-2 text-xs text-zinc-500 dark:text-zinc-300">
+                                    Found {searchResults.results.length} results
+                                    in {searchResults?.duration.toFixed(0)}ms
+                                 </article>
+                              </>
+                           ) : null}
+                        </CommandList>
+                     </Command>
+                  </DialogContent>
+               </Dialog>
+            </div>
+         </div>
+      </main>
+   )
 }
